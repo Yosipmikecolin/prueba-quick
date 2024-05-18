ae8f49793fcc1c18573576bf1e87c834148334f8
Yosip Mike Colin <frontend@agrak.com>
2024-05-18 10:31:17 -0500

    Adjusting pokemon view

--- src/views/pokemons/Pokemons.tsx
+++ src/views/pokemons/Pokemons.tsx
@@ -1,17 +1,36 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useMapQuery } from "../../api/queries/querie-pokemon";
 import classes from "./Pokemons.module.css";
 import { MapPokemons, TablePokemons } from "./components";
+import IconClose from "/images/icon-close-sesion.png";
+import { useNavigate } from "react-router";
+import { useAuth } from "../../hooks";
 
 const Pokemons: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const { setUser } = useAuth();
   const [map, setMap] = useState<L.Map | null>(null);
   const { data: pokemons, refetch, isLoading } = useMapQuery(currentPage);
+  const navigate = useNavigate();
+
+  const closeSesion = useCallback(() => {
+    setUser(false);
+    localStorage.removeItem("user");
+    navigate("/");
+  }, []);
 
   return (
     <section className={classes["container-pokemons"]}>
+      <div className={classes["card-header-map"]}>
+        <button
+          className={classes["button-close-sesion"]}
+          onClick={closeSesion}
+        >
+          <img src={IconClose} width={20} height={20} alt="icon-close" />
+        </button>
+      </div>
       <>
         <div className={classes["card-map"]}>
           {isLoading ? (
@@ -35,6 +54,21 @@ const Pokemons: React.FC = () => {
           )}
         </div>
       </>
+
+      <div className={classes["card-footer-map"]}>
+        <span>© Quick 2024</span>
+        <span>
+          Desarrollado por:
+          <strong>
+            <a
+              href="https://www.linkedin.com/in/yosip-mike-colin-parrado-6b22041bb/"
+              target="_blank"
+            >
+              Yosip Mike Colin
+            </a>
+          </strong>
+        </span>
+      </div>
     </section>
   );
 };
